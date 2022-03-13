918b6a7cc6899d85316a55eef818143eb922a56a
unknown <guilherme4garcia@gmail.com>
2022-03-13 16:52:38 -0300

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ControleRemoto c1 = new ControleRemoto();
+        c1.ligar();
+        c1.maisVolume();
+        c1.abrirMenu();
+
+    }
+}
